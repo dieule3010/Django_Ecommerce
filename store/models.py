4c2ef5375d3d05f7ef0f710dcb0d7e403f432cf1
Doanh2406 <quocdoanh02468@gmail.com>
2024-05-27 21:54:58 +0700

    update

--- store/models.py
+++ store/models.py
@@ -5,7 +5,9 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name
+      return self.name
+    class Meta:
+      verbose_name_plural = 'categories'
 # Customers
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
