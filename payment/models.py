0f4de12e8776fac3be672553fe1196d2efafe2dc
dieule3010 <ngocdieuks3010@gmail.com>
2024-09-21 22:37:43 +0700

    Customer Order Models

--- payment/models.py
+++ payment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from store.models import Product
 
 class ShippingAddress(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
@@ -18,3 +19,20 @@ class ShippingAddress(models.Model):
   def __str__(self):
     return f'Shipping Address - {str(self.id)}'
 
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null= True, blank=True)
+  full_name = models.CharField(max_length=250)
+  email = models.EmailField(max_length=250)
+  shipping_address = models.TextField(max_length=15000)
+  amount_paid = models.DecimalField(max_digits=7, decimal_places = 2)
+  date_odered = models.DateTimeField(auto_now_add=True)
+  def __str__(self):
+     return f'Order - {str(self.id)}'
+class OrderItem(models.Model):
+  order = models.ForeignKey(Order, on_delete=models.CASCADE, null= True)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE, null= True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
+  quantity = models.PositiveBigIntegerField(default=1)
+  price = models.DecimalField(max_digits=7, decimal_places=2)
+  def __str__(self):
+     return f'OrderItem - {str(self.id)}'
