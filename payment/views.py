e3cc19875c067a653443271581f02547cdecbbdc
dieule3010 <ngocdieuks3010@gmail.com>
2024-09-22 15:45:25 +0700

    Generate Orders

--- payment/views.py
+++ payment/views.py
@@ -2,16 +2,47 @@ from django.shortcuts import render, redirect
 from . import views
 from cart.cart import Cart
 from django.shortcuts import get_object_or_404
-from payment.models import ShippingAddress
+from payment.models import ShippingAddress, Order, OrderItem
 from payment.forms import ShippingForm, PaymentForm
+from django.contrib.auth.models import User
 from django.contrib import messages
 
+def process_order(request):
+    if request.POST:
+      cart = Cart(request) # Lấy giỏ hàng từ session hiện tại
+      cart_products = cart.get_prods
+      quantities = cart.get_quants
+      totals = cart.cart_total()
+      payment_form = PaymentForm(request.POST or None)
+      my_shipping = request.session.get('my_shipping')
+
+      full_name = my_shipping['shipping_full_name']
+      email = my_shipping['shipping_email']
+      shipping_address = f"{my_shipping['shipping_address1']}\n{my_shipping['shipping_address2']}\n{my_shipping['shipping_city']}\n{my_shipping['shipping_state']}\n{my_shipping['shipping_zipcode']}\n{my_shipping['shipping_country']}\n"
+      amount_paid = totals
+      if request.user.is_authenticated:
+        user = request.user
+        create_order = Order(user = user, full_name = full_name,email = email, shipping_address = shipping_address, amount_paid = amount_paid)
+        create_order.save()
+        messages.success(request, "Order Placed!")
+        return redirect ('home')
+      else:
+        create_order = Order( full_name = full_name,email = email, shipping_address = shipping_address, amount_paid = amount_paid)
+        create_order.save()
+
+        messages.success(request, "Order Placed!")
+        return redirect ('home')
+    else:
+      messages.success(request, "Access Denied")
+      return redirect ('home')
 def billing_info(request):
   if request.POST:
     cart = Cart(request) # Lấy giỏ hàng từ session hiện tại
     cart_products = cart.get_prods
     quantities = cart.get_quants
     totals = cart.cart_total()
+    my_shipping = request.POST
+    request.session['my_shipping'] = my_shipping
     if request.user.is_authenticated:
       billing_form = PaymentForm()
       return render(request, "payment/billing_info.html",{'cart_products':cart_products, "quantities":quantities, "totals" : totals, "shipping_info": request.POST, "billing_form": billing_form})
